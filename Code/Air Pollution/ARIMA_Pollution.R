7acadf6876da63de60c88ea0fb63e4833cbd922a
Slava <vlyubchich@users.noreply.github.com>
2024-12-19 16:20:32 -0500

    replicate ARIMA mse and coverage

--- Code/Air Pollution/ARIMA_Pollution.R
+++ Code/Air Pollution/ARIMA_Pollution.R
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 library(forecast)
 library(lubridate)
 library(dplyr)
@@ -6,7 +8,7 @@ library(scales)
 library(scoringRules)
 library(scoringutils)
 
-load('pm25_2009_2019_golden_horseshoe_sites.rda')
+load('Data/pm25_2009_2019_golden_horseshoe_sites.rda')
 #dat<-read.csv('pm25_2009_2019_golden_horseshoe_sites.csv')
 #dat<-dat[-2616,]
 
@@ -23,15 +25,16 @@ results_lower <- lapply(results_lower, FUN = function(x) {
   y
 } )
 names(results_lower) <- names(pm25_ts)
-results_mean <- results_upper <- results_lower
+results_obs <- results_mean <- results_upper <- results_lower
 # 95% parametric normal quantile for setting up lower/upper
 q <- qnorm(0.975)
 
 for(m in 1:n_ts) {
+  print(paste(m, Sys.time()))
   x <- pm25_ts[[m]]
   train <- x[dates_formatted < ymd("2019-01-01")]  # update train
   # fit only on the training data
-  fit <- auto.arima(train, 
+  fit <- auto.arima(train,
                     max.p = 10,
                     max.q = 10,
                     max.P = 10,
@@ -39,7 +42,7 @@ for(m in 1:n_ts) {
                     max.d = 5,
                     seasonal = FALSE,
                     ic = 'aicc')
-  
+
   for(j in 1:355) {
     new_dat <- x[dates_formatted >= (ymd("2009-01-01") + j - 1) &
                    dates_formatted < (ymd("2019-01-01") + j - 1)]
@@ -49,10 +52,41 @@ for(m in 1:n_ts) {
     results_lower[[m]][j, ] <- unlist(fore$pred) - q * unlist(fore$se)
     results_upper[[m]][j, ] <- unlist(fore$pred) + q * unlist(fore$se)
     results_mean[[m]][j, ] <- unlist(fore$pred)
+    results_obs[[m]][j, ] <- x[dates_formatted >= (ymd("2019-01-01") + j - 1) &
+                                 dates_formatted < (ymd("2019-01-01") + j + 9)]
   }
 }
 
-results_ARIMA <- vector("list", 3)
-results_ARIMA[[1]] <- results_mean
-results_ARIMA[[2]] <- results_lower
-results_ARIMA[[3]] <- results_upper
+# results_ARIMA <- vector("list", 3)
+# results_ARIMA[[1]] <- results_mean
+# results_ARIMA[[2]] <- results_lower
+# results_ARIMA[[3]] <- results_upper
+
+alpha = 0.05
+# Calculate performance metrics by station and forecast horizon
+PERF <- lapply(1:n_ts, function(m) {
+  # errors
+  e <- results_obs[[m]] - results_mean[[m]]
+  # mean squared error
+  mse <- apply(e^2, 2, mean)
+  # coverage
+  cvr <- apply((results_lower[[m]] <= results_obs[[m]]) & (results_obs[[m]] <= results_upper[[m]]), 2, mean)
+  # interval score
+  iscore <- results_upper[[m]] - results_lower[[m]] + 2 * (results_lower[[m]] - results_obs[[m]]) * as.numeric(results_obs[[m]] < results_lower[[m]]) / alpha + 2 * (results_obs[[m]] - results_upper[[m]]) * as.numeric(results_obs[[m]] > results_upper[[m]]) / alpha
+  iscore <- apply(iscore, 2, mean)
+  # combine results
+  tibble(mse = mse,
+         cvr = cvr,
+         iscore = iscore)
+})
+
+# Average across stations and horizons
+bind_rows(PERF) %>%
+  apply(2, mean)
+
+bind_rows(PERF) %>%
+  apply(2, sd)
+
+
+
+
