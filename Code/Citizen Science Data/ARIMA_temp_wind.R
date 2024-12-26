2472fac668d475a7fec5b5624e09757f605a4d38
Slava <vlyubchich@users.noreply.github.com>
2024-12-25 23:45:10 -0500

    replicate ARIMA temp citizen

--- Code/Citizen Science Data/ARIMA_temp_wind.R
+++ Code/Citizen Science Data/ARIMA_temp_wind.R
@@ -1,6 +1,6 @@
 #### Get data and divide into training and test
 
-load("ICData.rda")
+load("Data/ICData.rda")
 
 dat=station_int[station_int[,4]==12,]
 library(forecast)
@@ -45,20 +45,46 @@ for (i in 2:nrow(pred.wind)){
   wind.model2=Arima(c(train$wind.res,test$wind.res[1:(i-1)]),xreg=as.matrix(rbind(xreg.wind.train,xreg.wind.test[1:(i-1),])),model=wind.model)
   wind.1=data.frame(forecast(wind.model2,xreg=as.matrix(xreg.wind.test[i,]),h=1))
   pred.wind[i,]=c(wind.1$Point.Forecast,wind.1$Lo.95,wind.1$Hi.95)
-  
+
   temp.model2=Arima(c(train$tmpf.res,test$tmpf.res[1:(i-1)]),xreg=as.matrix(rbind(xreg.temp.train,xreg.temp.test[1:(i-1),])),model=temp.model)
   temp.1=data.frame(forecast(temp.model2,xreg=as.matrix(xreg.temp.test[i,]),h=1))
   pred.temp[i,]=c(temp.1$Point.Forecast,temp.1$Lo.95,temp.1$Hi.95)
-  
+
 }
 
 ### Actual values
   actual.temp=test$tmpf.res[1:(nrow(test)-1)]
   actual.wind=test$wind.res[1:(nrow(test)-1)]
-  
+
 ### Replace predicted negative values with 0
   pred.wind[pred.wind<0]<-0
-  
 
-  
-  
+
+
+
+  results_obs <- cbind(actual.temp)
+  results_mean <- pred.temp[,1, drop = FALSE]
+  results_lower <- pred.temp[,2, drop = FALSE]
+  results_upper <- pred.temp[,3, drop = FALSE]
+
+  alpha = 0.05
+  # Calculate performance metrics by station and forecast horizon
+  PERF <- lapply(1, function(m) {
+    # errors
+    e <- results_obs - results_mean
+    # mean squared error
+    mse <- apply(e^2, 2, mean)
+    # coverage
+    cvr <- apply((results_lower <= results_obs) & (results_obs <= results_upper), 2, mean)
+    # interval score
+    iscore <- (results_upper - results_lower) + 2 * (results_lower - results_obs) * as.numeric(results_obs < results_lower) / alpha + 2 * (results_obs - results_upper) * as.numeric(results_obs > results_upper) / alpha
+    iscore <- apply(iscore, 2, mean)
+    # combine results
+    tibble(MSE = mse,
+           Coverage = cvr,
+           iscore = iscore)
+  })
+
+
+
+
